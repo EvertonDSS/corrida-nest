cc75f7c5b9f356eb3b3fca17e87b99853c48c68d
Everton David <everton.097@gmail.com>
2025-09-22 13:31:00 -0300

    Adicionar o pacote @nestjs/serve-static e configurar o uso de ativos estáticos no Swagger na aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
+// src/main.ts
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { join } from 'path';
+
+// 1. Importe o tipo específico da plataforma Express
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Minha API')
-    .setDescription('Documentação da API com Swagger')
-    .setVersion('1.0')
-    .addBearerAuth() // se usar JWT
-    .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api/docs', app, document);
+  // 2. Especifique o tipo <NestExpressApplication> aqui
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.useStaticAssets(join(__dirname, '..', 'swagger-ui-dist'), {
+    prefix: '/api/docs/',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Corrida API')
+    .setDescription('Documentação da API de Corridas')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
